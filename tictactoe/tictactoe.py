f63f54ffc3d023d698e8e3f8f62c9b1c71012d4b
Studious Gamer <67009562+studiousgamer@users.noreply.github.com>
2021-06-28 09:28:54 +0530

    Update tictactoe.py
    Shortened the code :D
    Tested

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -34,152 +34,42 @@ def tictactoe_game():
     print(" " + "|" + table[6] + "|" + table[7] + "|" + table[8] + "|")
     print("")
 
+def moving(num, player):
+    table[int(num)] = player
+    cs = 'cls'
+    os.system(cs)
+    tictactoe_game()
 
-def playerx():
-    playerx = input("PX:")
+def player1():
+    ActivePlayer=True
+    while ActivePlayer:
+        playerx = input("PX:")
+        for i in range(9):
+            if playerx == moves[i]:
+                moving(i, "X")
+                ActivePlayer = False
+        if playerx == moves[10]:
+            cs = 'cls'
+            os.system(cs)
+
+def player2():
     ActivePlayer = True
-    if playerx not in moves:
-        tictactoe_main()
-    if playerx == moves[0]:
-        table[0] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[1]:
-        table[1] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[2]:
-        table[2] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[3]:
-        table[3] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[4]:
-        table[4] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[5]:
-        table[5] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[6]:
-        table[6] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[7]:
-        table[7] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[8]:
-        table[8] = "X"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playerx == moves[10]:
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-
-    while ActivePlayer is False:
-        playery()
-
-    
-def playery():
-    playery = input("PY:")
-    ActivePlayer = True
-    if playery not in moves:
-        tictactoe_main()
-    if playery == moves[0]:
-        table[0] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[1]:
-        table[1] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[2]:
-        table[2] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[3]:
-        table[3] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[4]:
-        table[4] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[5]:
-        table[5] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[5]:
-        table[5] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[6]:
-        table[6] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[7]:
-        table[7] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[8]:
-        table[8] = "Y"
-        ActivePlayer = False
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-    if playery == moves[10]:
-        cs = 'cls'
-        os.system(cs)
-        tictactoe_main()
-
-    while ActivePlayer is False:
-        playerx()
-
+    while ActivePlayer:
+        playery = input("PY:")
+        for i in range(9):
+            if playery == moves[i]:
+                moving(i, "Y")
+                ActivePlayer = False
+        if playery == moves[10]:
+            cs = 'cls'
+            os.system(cs)
+            tictactoe_game()
 
 def tictactoe_main():
     tictactoe_game()
-    playery()
+    while True:
+        player2()
+        player1()
 
 
 tictactoe_main()
